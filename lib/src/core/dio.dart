bf7a29658ad1d02fc94edcc55c3de30d13ffb9a4
Afief Sholichuddin <apiep@users.noreply.github.com>
2020-04-20 13:30:41 +0700

    add missing dio.dart files

---
+++ lib/src/core/dio.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:qiscus_chat_sdk/src/core/core.dart';
+
+Dio getDio(Storage storage, Logger logger) {
+  final interceptor = InterceptorsWrapper(
+    onRequest: (request) {
+      request.baseUrl = '${storage?.baseUrl}/api/v2/mobile/';
+      request.headers['qiscus-sdk-app-id'] = storage.appId;
+      request.headers['qiscus-sdk-version'] = 'dart-alpha1.0';
+      if (storage?.token != null) {
+        request.headers['qiscus-sdk-token'] = storage.token;
+        request.headers['qiscus-sdk-user-id'] = storage.userId;
+      }
+      return request;
+    },
+  );
+  var dio = Dio(BaseOptions())
+    ..interceptors.addAll([
+      interceptor,
+    ]);
+  if (logger.enabled) {
+    dio.interceptors.add(PrettyDioLogger(
+      requestBody: true,
+      requestHeader: true,
+    ));
+  }
+  return dio;
+}
