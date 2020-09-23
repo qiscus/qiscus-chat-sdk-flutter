55baad9d284480072012979fade94cb2464ba166
Afief Sholichuddin <apiep.oedin@gmail.com>
2020-09-23 17:36:03 +0700

    add more test

--- test/src/features/message/message_api_request_test.dart
+++ test/src/features/message/message_api_request_test.dart
@@ -1,3 +1,4 @@
+import 'package:qiscus_chat_sdk/src/core.dart';
 import 'package:qiscus_chat_sdk/src/features/message/message.dart';
 import 'package:test/test.dart';
 
@@ -7,7 +8,10 @@ void main() {
       message: 'message',
       roomId: 1,
     );
-    test('body', () {});
+    test('body', () {
+      expect(request.url, 'post_comment');
+      expect(request.method, IRequestMethod.post);
+    });
     test('format', () {});
   });
 }
---
+++ test/src/features/realtime/usecase/realtime_test.dart
@@ -0,0 +1,45 @@
+import 'package:mockito/mockito.dart';
+import 'package:qiscus_chat_sdk/src/core.dart';
+import 'package:qiscus_chat_sdk/src/features/realtime/realtime.dart';
+import 'package:test/test.dart';
+
+class MockService extends Mock implements IRealtimeService {}
+
+void main() {
+  IRealtimeService service;
+  OnConnected onConnected;
+  OnReconnecting onReconnecting;
+  OnDisconnected onDisconnected;
+
+  setUpAll(() {
+    service = MockService();
+    onConnected = OnConnected(service);
+    onReconnecting = OnReconnecting(service);
+    onDisconnected = OnDisconnected(service);
+  });
+
+  test('OnConnected.subscribe', () async {
+    when(service.onConnected()).thenAnswer((_) => Stream.value(null));
+
+    await onConnected.subscribe(noParams).run();
+
+    verify(service.onConnected()).called(1);
+    verifyNoMoreInteractions(service);
+  });
+  test('OnReconnecting.subscribe', () async {
+    when(service.onReconnecting()).thenAnswer((_) => Stream.value(null));
+
+    await onReconnecting.subscribe(noParams).run();
+
+    verify(service.onReconnecting()).called(1);
+    verifyNoMoreInteractions(service);
+  });
+  test('OnConnected.subscribe', () async {
+    when(service.onDisconnected()).thenAnswer((_) => Stream.value(null));
+
+    await onDisconnected.subscribe(noParams).run();
+
+    verify(service.onDisconnected()).called(1);
+    verifyNoMoreInteractions(service);
+  });
+}
